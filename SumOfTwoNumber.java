1074df870e47beef55aa28f6bd06c57c0dcbbcf2
Shoaib Ul Hassan <shoaibulhassaan@gmail.com>
2023-11-24 20:42:49 +0500

    Create SumOfTwoNumber
    Console base create class name SumOfTwoNumber

---
+++ SumOfTwoNumber.java
@@ -0,0 +1,13 @@
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        // Declare and initialize two numbers
+        int a = 5;
+        int b = 10;
+
+        // Calculate the sum
+        int sum = a + b;
+
+        // Print the result
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
