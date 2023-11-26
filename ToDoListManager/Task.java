b26b4a71250a08a47466260e9879f2084da62c1a
Shoaib Ul Hassan <shoaibulhassaan@gmail.com>
2023-11-26 12:34:48 +0500

    Create Task.java
    task class

---
+++ ToDoListManager/Task.java
@@ -0,0 +1,9 @@
+class Task {
+    String description;
+    boolean isCompleted;
+
+    Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+}
